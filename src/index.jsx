d12a4a4da32b62dab510e52836c363f6d88c3a61
xiongjia <lexiongjia@gmail.com>
2018-03-03 11:09:06 +0800

    adding redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
 import App from './component/app.jsx';
 import { Context } from './misc.js';
 
 import 'bootstrap';
 import './main.scss';
 
+const store = createStore(reducers);
 const context = new Context();
 
 ReactDOM.render(
-  <App
-    context={context}
-  />,
+  <Provider store={store}>  
+    <App context={context} />
+  </Provider>,
   document.getElementById('root')
 );
